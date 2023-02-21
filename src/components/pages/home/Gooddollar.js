d2fce764825c96d6b2e3e49c3481fcb784679f41
Alex_Molotsylo <motzart66@gmail.com>
2023-02-21 17:55:17 +0200

    check goodDollar url params

--- src/components/pages/home/Gooddollar.js
+++ src/components/pages/home/Gooddollar.js
@@ -107,7 +107,6 @@ export const Gooddollar = () => {
   useEffect(() => {
     if (window.location.href.includes("?login")) {
       // TODO here we avoid double encode URI and change incorrect symbols, fast workaround
-      console.log('check url params', window.location.href);
       window.history.replaceState({}, '', window.location.href.replace('%253D', '='));
       setShowStep(2);
     }
