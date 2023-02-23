bf92db1e77841e402899849710988a0f9af21f79
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2023-02-24 02:11:23 +0530

    changes

--- src/components/pages/home/Gooddollar.js
+++ src/components/pages/home/Gooddollar.js
@@ -123,7 +123,7 @@ export const Gooddollar = () => {
   useEffect(() => {
     if (window.location.href.includes("?login")) {
       // TODO here we avoid double encode URI and change incorrect symbols, fast workaround
-      window.history.replaceState({}, '', window.location.href.replace('%253D', '='));
+      // window.history.replaceState({}, '', window.location.href.replace('%253D', '='));
       setShowStep(2);
     }
   }, []);
