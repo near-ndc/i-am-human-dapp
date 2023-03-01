71620f4a6bd79d2db955f26469120fc559f925e4
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2023-03-02 01:38:13 +0530

    changes

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -67,12 +67,17 @@ export const Header = ({ isAdmin, setShowAdmin }) => {
             <div className="hidden lg:flex lg:min-w-0 space-x-4 lg:justify-end">
               {isSignedIn && (
                 <>
-                  {/* <button
+                  <button
                     onClick={async () => {
                       await supabase
                         .from("users")
                         .delete()
                         .match({ wallet_identifier: wallet.accountId });
+                      await wallet.callMethod({
+                        contractId: "community-sbt-1.i-am-human.testnet",
+                        method: "revoke_for",
+                        args: { accounts: [wallet.accountId], metadata: {} },
+                      });
                       toast.info("Your account info has been reset");
                       setTimeout(() => {
                         window.location.reload();
@@ -81,7 +86,7 @@ export const Header = ({ isAdmin, setShowAdmin }) => {
                     className="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20 flex items-center space-x-2"
                   >
                     <p>Reset Account</p>
-                  </button> */}
+                  </button>
                   {isAdmin ? (
                     <button
                       onClick={() => setShowAdmin(true)}
