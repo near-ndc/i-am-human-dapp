2c881b8dc0658d57fbbe2b2d1687cd15b27b421c
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2023-04-25 18:54:19 +0530

    minor-gooddollar-fix

--- src/components/pages/landing/Gooddollar.js
+++ src/components/pages/landing/Gooddollar.js
@@ -150,7 +150,10 @@ export const Gooddollar = ({ setShowGooddollarVerification }) => {
             ...d,
             mobile: !Boolean(d?.mobile?.value),
           }));
-          const isVerified = d?.isAddressWhitelisted?.value;
+          console.log(d?.isAddressWhitelisted);
+          const isVerified =
+            d?.isAddressWhitelisted?.value === true ||
+            d?.isAddressWhitelisted?.isVerified === true;
           setValues({
             gDollarAccount: d?.walletAddress?.value,
             status: isVerified ? 'Whitelisted' : 'Not Whitelisted',
