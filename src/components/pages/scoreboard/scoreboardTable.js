844c2af907f6291ef665956e5da08eed835b4004
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2023-05-15 01:33:56 +0530

    scoreboard-fixes

--- src/components/pages/scoreboard/scoreboardTable.js
+++ src/components/pages/scoreboard/scoreboardTable.js
@@ -25,20 +25,8 @@ export function ScoreBoardTable({ communities = [] }) {
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
           <h1 className="text-base font-semibold leading-6 text-gray-900">
-            Users
+            Community Scoreboard
           </h1>
-          <p className="mt-2 text-sm text-gray-700">
-            A list of all the users in your account including their name, title,
-            email and role.
-          </p>
-        </div>
-        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-          <button
-            type="button"
-            className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Add user
-          </button>
         </div>
       </div>
       <div className="mt-8 flow-root">
