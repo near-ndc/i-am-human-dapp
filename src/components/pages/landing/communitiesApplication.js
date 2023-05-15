63388949b444c104145f734d0845eef181498bd4
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2023-05-15 21:40:13 +0530

    community-application-fixes

--- src/components/pages/landing/communitiesApplication.js
+++ src/components/pages/landing/communitiesApplication.js
@@ -57,7 +57,7 @@ export const CommunitiesApplication = ({ isSignedIn }) => {
                     <div role="status">
                       <svg
                         aria-hidden="true"
-                        class="w-5 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                        class="w-5 h-5 mr-2 text-gray-200 animate-spin fill-blue-600"
                         viewBox="0 0 100 101"
                         fill="none"
                         xmlns="http://www.w3.org/2000/svg"
