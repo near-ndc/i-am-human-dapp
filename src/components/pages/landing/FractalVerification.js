b9e8784ec530daf8d7e80c83ae22354296aad9b4
Aziz <35035144+AzizAbdullaev@users.noreply.github.com>
2023-05-26 18:34:07 +0200

    Added new Connect wallet UI (#67)
    Co-authored-by: AAbdullaev <azizbek.abdullaev@onseostudio.com>

--- src/components/pages/landing/FractalVerification.js
+++ src/components/pages/landing/FractalVerification.js
@@ -1,35 +1,59 @@
 import React, { useState, useEffect } from 'react';
-import { useFormik } from 'formik';
 import { toast } from 'react-toastify';
-import * as Yup from 'yup';
 import { CircleSpinner } from 'react-spinners-kit';
 import 'react-phone-number-input/style.css';
 import 'react-phone-number-input/style.css';
 import { wallet } from '../../..';
 import { verifyUser } from '../../../services/api';
-import { useUniqueGUser } from '../../../utils/uniqueUser';
 import { supabase } from '../../../utils/supabase';
 import { log_event } from '../../../utils/utilityFunctions';
 import { getConfig } from '../../../utils/config';
+import { WalletSVG } from '../../../images/WalletSVG';
+import { FaceSVG } from '../../../images/FaceSVG';
+import { MintSVG } from '../../../images/MintSVG';
+import { Warning } from '../../../images/Warning';
+import FVSBTImage from '../../../images/FvSBT.png';
+import COMMUNITYImage from '../../../images/COMMUNITY.png';
+import FACE_VERIFICATIONImage from '../../../images/FACE_VERIFICATION.png';
+import NO_KNOWLEDGE_KYCImage from '../../../images/NO_KNOWLEDGE_KYC.png';
+import ORIGINAL_MEMBERImage from '../../../images/ORIGINAL_MEMBER.png';
+
+const TOKENS_PLACEHOLDER = [
+  COMMUNITYImage,
+  FACE_VERIFICATIONImage,
+  NO_KNOWLEDGE_KYCImage,
+  ORIGINAL_MEMBERImage,
+];
 
 export const ConnectWallet = () => (
-  <div className="mt-8">
-    <p className="text-s mb-5">
-      Near wallet is required. Be sure you connect the wallet you want to use
-      for governance. Get a new wallet for free if you don't already have one
-      (or need an alt).
+  <div className="w-full">
+    <div className="flex items-center justify-center w-20 h-20 rounded-full border-2 border-purple-400">
+      <div className="flex items-center justify-center w-full h-full rounded-full border-2 border-purple-500 bg-purple-200 shadow-[inset_0_0px_4px_#FFFFFF] shadow-purple-400">
+        <WalletSVG styles="w-12 h-12 fill-purple-400 stroke-purple-200" />
+      </div>
+    </div>
+    <h2 className="text-4xl font-bold	my-4">Connect Wallet</h2>
+    <p className="text-s mb-8">
+      A NEAR wallet is required. Be sure you connect an account you want to use
+      for <br /> governance. Get a new account if you don't already have one.
     </p>
     <button
       onClick={() => wallet.signIn()}
       type="button"
-      className="flex items-center mx-auto rounded border border-transparent bg-indigo-600 w-60 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      className="rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700"
     >
       <p className="mx-auto w-[fit-content]">Connect Wallet</p>
     </button>
   </div>
 );
 
-export const MintSBT = ({ setActiveTabIndex }) => {
+export const MintSBT = ({
+  setActiveTabIndex,
+  setError,
+  isError,
+  setSuccessSBT,
+  successSBT,
+}) => {
   const [editableFields, setEditableFields] = useState({
     code: '',
     claimer: wallet.accountId,
@@ -37,74 +61,60 @@ export const MintSBT = ({ setActiveTabIndex }) => {
   });
   const [submit, setSubmit] = useState(null);
 
-  const { values, handleSubmit, handleBlur, handleChange } = useFormik({
-    initialValues: {
-      name: '',
-      gDollarAccount: '',
-      status: '',
-    },
-    validationSchema: Yup.object().shape({
-      email: Yup.string().email('Invalid email'),
-    }),
-    onSubmit: async (data) => {
-      // here need to clear url and remove all unnecessary data from url for near wallet redirect
-      window.history.replaceState({}, '', window.location.origin);
-      setSubmit(true);
-      let updateData = {
+  const mintSBT = async () => {
+    window.history.replaceState({}, '', window.location.origin);
+    setSubmit(true);
+    let updateData = {
+      wallet_identifier: wallet.accountId,
+      ...editableFields,
+    };
+    log_event({
+      event_log: `Data sent to verify API ${JSON.stringify(editableFields)}`,
+    });
+    try {
+      const result = await verifyUser(editableFields);
+      if (result?.error) {
+        setError(true);
+        return;
+      }
+
+      const { data } = await supabase.select('users', {
         wallet_identifier: wallet.accountId,
-        g$_address: data.gDollarAccount,
-        status: 'Approved',
-      };
-      log_event({
-        event_log: `Data sent to verify API ${JSON.stringify(editableFields)}`,
       });
-      try {
-        const result = await verifyUser(editableFields);
-        const { data } = await supabase.select('users', {
+      log_event({
+        event_log: `Data receivied from verify API ${JSON.stringify(result)}`,
+      });
+      if (data?.[0]) {
+        await supabase.update('users', updateData, {
           wallet_identifier: wallet.accountId,
         });
-        log_event({
-          event_log: `Data receivied from verify API ${JSON.stringify(result)}`,
-        });
-        if (data?.[0]) {
-          await supabase.update('users', updateData, {
-            wallet_identifier: wallet.accountId,
-          });
-        } else {
-          await supabase.insert('users', updateData);
-        }
-        log_event({ event_log: 'Applied for FV SBT' });
-        const { mintFee, fractal_contract } = getConfig();
-        await wallet.callMethod({
-          contractId: fractal_contract,
-          method: 'sbt_mint',
-          args: {
-            claim_b64: result.m,
-            claim_sig: result.sig,
-          },
-          deposit: mintFee,
-        });
-      } catch (e) {
-        log_event({
-          event_log: `Error happened while submitting FB SBT ${JSON.stringify(
-            e
-          )}`,
-        });
-        toast.error(
-          'An error occured while submitting your details , please try again'
-        );
-      } finally {
-        setSubmit(false);
+      } else {
+        await supabase.insert('users', updateData);
       }
-    },
-  });
-
-  const handleValues = (key) => ({
-    value: values[key],
-    disabled: editableFields?.[key],
-    onChange: handleChange(key),
-    onBlur: handleBlur(key),
-  });
+      log_event({ event_log: 'Applied for FV SBT' });
+      const { mintFee, fractal_contract } = getConfig();
+      await wallet.callMethod({
+        contractId: fractal_contract,
+        method: 'sbt_mint',
+        args: {
+          claim_b64: result.m,
+          claim_sig: result.sig,
+        },
+        deposit: mintFee,
+      });
+    } catch (e) {
+      log_event({
+        event_log: `Error happened while submitting FB SBT ${JSON.stringify(
+          e
+        )}`,
+      });
+      toast.error(
+        'An error occured while submitting your details , please try again'
+      );
+    } finally {
+      setSubmit(false);
+    }
+  };
 
   useEffect(() => {
     const URL = window.location;
@@ -118,91 +128,128 @@ export const MintSBT = ({ setActiveTabIndex }) => {
     }
   }, []);
 
-  const { isExistingGUser, loading: isGLoading } = useUniqueGUser({
-    gAddress: values.gDollarAccount,
-  });
-
   return (
-    <div className="p-2 w-full">
-      <div className="mt-14">
-        <p className="text-l mt-2">
-          To get your I Am Human SBT you must first connect a Near wallet and
-          complete the fractal face scan. Revisit this step after completing
-          both of the prerequisits!
-        </p>
-
-        <form
-          onSubmit={(e) => handleSubmit(e)}
-          className="flex justify-center font-light tracking-wider mt-3 mb-16"
-        >
-          {values.status === 'Whitelisted' && (
-            <>
-              <div className="flex items-center justify-between">
-                <p className="w-[120px]">G$ Account:</p>
-                <input
-                  className="w-[88%] bg-gray-100 p-1 rounded px-3"
-                  placeholder="Account Address"
-                  {...handleValues('gDollarAccount')}
-                />
-              </div>
-              <div className="flex items-center justify-between">
-                <p className="w-[120px]">Status:</p>
-                <input
-                  className="w-[88%] bg-gray-100 p-1 rounded px-3"
-                  placeholder="Status"
-                  {...handleValues('status')}
-                />
-              </div>
-            </>
-          )}
-
-          {isGLoading ? (
-            <>
-              <button className="bg-blue-600 w-40 mt-3 text-white rounded shadow-lg font-medium w-[fit-content] text-sm px-4 py-2 float-right">
-                <div className="w-[fit-content] mx-auto">
-                  <CircleSpinner size={20} />
+    <>
+      <div className="w-full flex justify-between items-center">
+        <div>
+          <div
+            className={`flex items-center justify-center w-20 h-20 rounded-full border-2 ${
+              successSBT ? 'border-green-400' : 'border-purple-400'
+            }`}
+          >
+            <div
+              className={`flex items-center justify-center w-full h-full rounded-full border-2 ${
+                successSBT
+                  ? 'border-green-500 bg-green-200 shadow-green-400'
+                  : 'border-purple-500 bg-purple-200 shadow-purple-400'
+              } shadow-[inset_0_0px_4px_#FFFFFF]`}
+            >
+              <MintSVG
+                styles={`w-12 h-12 ${
+                  successSBT ? 'stroke-green-300' : 'stroke-purple-400'
+                }`}
+              />
+            </div>
+          </div>
+          <h2 className="text-4xl font-bold	my-4">
+            {successSBT
+              ? 'Success!'
+              : 'Mint Face Verification Soul Bound Token'}
+          </h2>
+          <p className="text-s mb-8">
+            {successSBT
+              ? 'Check out your newly minted Soul Bound Tokens! You can now participate in Near Digital Collective (NDC) governance. Share the good news!'
+              : "Congratulations! You're eligible to receive Soul Bound Tokens (SBTs) that verify that you are a human."}
+          </p>
+          {successSBT && (
+            <div className="grid grid-rows-2 grid-flow-col gap-6 mb-8">
+              {TOKENS_PLACEHOLDER.map((item) => (
+                <div className="bg-gray-100 p-2 flex items-center rounded-2xl">
+                  <div className="w-44 h-44 flex">
+                    <img src={item} className="rounded-2xl" />
+                  </div>
+                  <div className="">
+                    <p className="">Token ID: 113</p>
+                    <p className="my-3">Issed on: 10 May 2023</p>
+                    <p className="mb-3">Expired on: 09 May 2024</p>
+                    <p className="">Days until expiration: 351</p>
+                  </div>
                 </div>
-              </button>
-            </>
-          ) : isExistingGUser ? (
+              ))}
+            </div>
+          )}
+          {isError ? (
             <>
-              <p>This Gooddollar account is already registered with us.</p>
+              <div className="flex">
+                <button
+                  onClick={() => mintSBT()}
+                  type="button"
+                  className="rounded-md bg-red-500 px-4 py-2 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700"
+                >
+                  {submit ? (
+                    <CircleSpinner size={20} />
+                  ) : (
+                    <p className="mx-auto w-[fit-content]">Try Again</p>
+                  )}
+                </button>
+                <div className="rounded-md px-4 py-2 text-base font-medium text-red-500 shadow-sm bg-red-100 ml-3 flex">
+                  <Warning />
+                  <p className="ml-2">
+                    Something went wrong, please try again.
+                  </p>
+                </div>
+              </div>
               <button
+                onClick={() => {
+                  setSuccessSBT(true);
+                  setError(false);
+                }}
                 type="button"
-                onClick={() => null}
-                className="bg-blue-600 mt-3 text-white rounded shadow-lg font-medium w-[fit-content] text-sm px-4 py-2"
+                className="rounded-md bg-yellow-400 px-4 py-2 mt-3 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700"
               >
-                Ok
+                <p className="mx-auto w-[fit-content]">
+                  Show Success Screen (only for test)
+                </p>
               </button>
             </>
           ) : (
             <button
-              type="submit"
-              onClick={() => {
-                log_event({
-                  event_log: 'Started Application flow for gooddollar',
-                });
-              }}
-              disabled={submit}
-              className="bg-blue-600 w-40 mt-3 text-white rounded shadow-lg font-medium w-[fit-content] text-sm px-4 py-2 float-right"
+              onClick={() => !successSBT && mintSBT()}
+              type="button"
+              className="rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700"
             >
-              {!submit ? (
-                'Mint SBT'
+              {submit ? (
+                <CircleSpinner size={20} />
               ) : (
-                <div className="w-[fit-content] mx-auto">
-                  <CircleSpinner size={20} />
-                </div>
+                <p className="mx-auto w-[fit-content]">
+                  {successSBT ? 'Share on Twitter' : 'Connect Wallet'}
+                </p>
               )}
             </button>
           )}
-        </form>
+        </div>
+        {!successSBT && (
+          <div className="md:min-w-[250px] order-first md:order-last w-full md:w-1/3 flex justify-center">
+            <img src={FVSBTImage} className="object-fill" />
+          </div>
+        )}
       </div>
-    </div>
+
+      {successSBT && (
+        <p className="text-s mt-8">
+          Please note that you can request for your SBTs to be revoked (along
+          with deletion of any identifying data stored by I-AM-HUMAN and
+          Fractal).
+        </p>
+      )}
+    </>
   );
 };
 
 export const ScanFace = () => {
+  const [submit, setSubmit] = useState(null);
   const fractalLoginCb = () => {
+    setSubmit(true);
     const { fractal_link, fractal_client_id, succes_fractal_state } =
       getConfig();
     const fractalVerifyURL =
@@ -223,39 +270,43 @@ export const ScanFace = () => {
   };
 
   return (
-    <div className="p-2 w-full">
-      <div className="w-full mt-14">
-        <p className="text-l mt-2">
-          To complete the face scan, create a new Fractal account or log into an
-          existing one. Please use the same email you previously used with
-          Fractal if you already have an account.
-        </p>
-        <p className="text-l mt-5">
-          While on the Fractal website, you'll need to follow the.
-        </p>
-        <p className="text-l my-5">
-          Have your phone handy in case Face Verification fails on desktop.
-          Return to I-AM-HUMAN when you've completed the verification on
-          Fractal.
-        </p>
-        <div
-          onClick={() =>
-            log_event({ event_log: 'Fractal authorization started' })
-          }
-        >
-          <button
-            onClick={() => fractalLoginCb()}
-            type="button"
-            className="flex items-center mx-auto rounded border border-transparent bg-indigo-600 w-60 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          >
-            <p className="mx-auto w-[fit-content]">Start Face Scan</p>
-          </button>
+    <div className="w-full">
+      <div className="flex items-center justify-center w-20 h-20 rounded-full border-2 border-purple-400">
+        <div className="flex items-center justify-center w-full h-full rounded-full border-2 border-purple-500 bg-purple-200 shadow-[inset_0_0px_4px_#FFFFFF] shadow-purple-400">
+          <FaceSVG styles="w-12 h-12 fill-purple-400 stroke-purple-200" />
         </div>
-        <p className="text-l font-light italic mt-5">
-          Fractal is a web3 identity provider. Your images are not stored, only
-          a “hash” of your face for uniqueness comparison with other users.
-        </p>
       </div>
+      <h2 className="text-4xl font-bold	my-4">Face Scan</h2>
+      <p className="text-s mb-8">
+        For the face scan, please create or log into your Fractal account.
+        Follow the steps on <br /> the Fractal website to complete the
+        verification.
+        <br />
+        <br />
+        Already have a Fractal account? Use the same email you previously used
+        with <br />
+        Fractal.
+        <br />
+        <br />
+        Keep your phone nearby in case Face Verification fails on desktop. Once
+        you've <br /> finished the verification on Fractal, return to
+        I-AM-HUMAN.
+      </p>
+      <button
+        onClick={() => fractalLoginCb()}
+        type="button"
+        className="rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700"
+      >
+        {submit ? (
+          <CircleSpinner size={20} />
+        ) : (
+          <p className="mx-auto w-[fit-content]">Start Face Scan</p>
+        )}
+      </button>
+      <p className="font-light italic mt-4 text-sm">
+        * Fractal is a web3 identity provider. Your images are not stored, only
+        a “hash” of your face for uniqueness comparison with other users.{' '}
+      </p>
     </div>
   );
 };
---
+++ src/pages/unAuth/IsSignedInLanding.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import {
+  ConnectWallet,
+  MintSBT,
+  ScanFace,
+} from '../../components/pages/landing/FractalVerification';
+import { Tabs } from '../../components/common/Tabs';
+import { WalletSVG } from '../../images/WalletSVG';
+import { FaceSVG } from '../../images/FaceSVG';
+import { MintSVG } from '../../images/MintSVG';
+
+export const IsSignedInLanding = ({ activeTabIndex, setActiveTabIndex }) => {
+  const [error, setError] = useState(false);
+  const [successSBT, setSuccessSBT] = useState(false);
+  const activeTab = (index) =>
+    activeTabIndex >= index ? 'stroke-purple-400' : 'stroke-gray-300';
+
+  const TabsData = [
+    {
+      name: 'Connect Wallet',
+      header: <WalletSVG styles={`w-12 h-12 ${activeTab(0)}`} />,
+      content: <ConnectWallet />,
+    },
+    {
+      name: 'Face Scan',
+      header: <FaceSVG styles={`w-12 h-12 ${activeTab(1)}`} />,
+      content: <ScanFace setError={setError} isError={error} />,
+    },
+    {
+      name: 'Mint SBT',
+      header: <MintSVG styles={`w-12 h-12 ${activeTab(2)}`} />,
+      content: (
+        <MintSBT
+          setActiveTabIndex={setActiveTabIndex}
+          successSBT={successSBT}
+          setSuccessSBT={setSuccessSBT}
+          setError={setError}
+          isError={error}
+        />
+      ),
+    },
+  ];
+
+  return (
+    <div className="lg:mx-auto lg:max-w-7xl">
+      <Tabs
+        tabs={TabsData}
+        activeTabIndex={activeTabIndex}
+        error={error}
+        successSBT={successSBT}
+        setActiveTabIndex={() => null}
+      />
+    </div>
+  );
+};
