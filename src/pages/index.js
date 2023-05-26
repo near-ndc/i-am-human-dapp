5676d82057509df8b9c70b74a3b26ae80d35923e
AAbdullaev <azizbek.abdullaev@onseostudio.com>
2023-05-26 18:30:01 +0200

    Enable Connect Wallet flow

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,12 @@ import { Footer } from '../components/common/footer';
 import Design from '../images/backLines.png';
 import { Landing } from './unAuth';
 import { Home } from './auth/home';
+import { IsSignedInLanding } from './unAuth/IsSignedInLanding';
 
 export function IndexPage({ isSignedIn }) {
   const [showAdmin, setShowAdmin] = useState(false);
+  const [showConnectWallet, setShowConnectWallet] = useState(false);
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
 
   useEffect(() => {
     const search = window.location.search;
@@ -19,6 +22,9 @@ export function IndexPage({ isSignedIn }) {
     if (community && vertical) {
       localStorage.setItem(CommunityDataKeys.COMMUNITY_NAME, community);
       localStorage.setItem(CommunityDataKeys.COMMUNITY_VERTICAL, vertical);
+    }
+    if (isSignedIn) {
+      setActiveTabIndex(1);
     }
   }, []);
 
@@ -85,67 +91,76 @@ export function IndexPage({ isSignedIn }) {
   return (
     <div className="isolate bg-white mx-auto max-w-7xl px-5 pt-10">
       <Header setShowAdmin={setShowAdmin} isAdmin={false} />
-      <div className="mt-[80px] md:mt-[100px] flex flex-col gap-y-32">
-        <div className="flex flex-wrap gap-10">
-          <div className="flex-1 min-w-[300px]">
-            <h1 className="font-bold text-4xl">
-              Get your Proof of Personhood with I-AM-HUMAN
-            </h1>
-            <p className="my-5">
-              Welcome, I-AM-HUMAN is your launchpad for several different types
-              of Soul Bound Tokens (SBTs). Each of which will identify you as a
-              human. With enough of these SBTs, you will have a strong
-              proof-of-personhood, which give you access to voting on
-              governance, on-chain reputation, DAOs, grassroots funding, and
-              much more.
-            </p>
-            <p className="">All you need to do is 3 easy steps</p>
-            <div className="grid grid-cols-3 my-5 gap-5">
-              {TabsData.map((tab, index) => {
-                return (
-                  <div>
-                    <div className="flex items-center gap-5">
-                      <div className="rounded-full bg-gradient-to-r from-purple-600 to-indigo-600 w-fit p-1">
-                        {tab.header}
-                      </div>
-                      {index < 2 ? (
-                        <hr class="h-px my-8 bg-gradient-to-r from-purple-600 to-indigo-600 border-0 w-full" />
-                      ) : (
-                        <span></span>
-                      )}
-                    </div>
-                    <p className="col-span-2 text-md mt-2">{tab.name}</p>
-                  </div>
-                );
-              })}
-            </div>
+      {!showConnectWallet ? (
+        <>
+          <div className="mt-[80px] md:mt-[100px] flex flex-col gap-y-32">
             <div className="flex flex-wrap gap-10">
-              <button
-                // onClick={() => setIsStarted(true)}
-                className="rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700"
-              >
-                Get Started
-              </button>
-              <button
-                onClick={() =>
-                  window.open(
-                    'https://i-am-human.gitbook.io/i-am-human-docs/',
-                    '_blank'
-                  )
-                }
-                className="rounded-md border border-purple-500 text-purple-500 border-1 px-4 py-2 text-base font-light text-black shadow-sm"
-              >
-                Learn More
-              </button>
+              <div className="flex-1 min-w-[300px]">
+                <h1 className="font-bold text-4xl">
+                  Get your Proof of Personhood with I-AM-HUMAN
+                </h1>
+                <p className="my-5">
+                  Welcome, I-AM-HUMAN is your launchpad for several different
+                  types of Soul Bound Tokens (SBTs). Each of which will identify
+                  you as a human. With enough of these SBTs, you will have a
+                  strong proof-of-personhood, which give you access to voting on
+                  governance, on-chain reputation, DAOs, grassroots funding, and
+                  much more.
+                </p>
+                <p className="">All you need to do is 3 easy steps</p>
+                <div className="grid grid-cols-3 my-5 gap-5">
+                  {TabsData.map((tab, index) => {
+                    return (
+                      <div>
+                        <div className="flex items-center gap-5">
+                          <div className="rounded-full bg-gradient-to-r from-purple-600 to-indigo-600 w-fit p-1">
+                            {tab.header}
+                          </div>
+                          {index < 2 ? (
+                            <hr class="h-px my-8 bg-gradient-to-r from-purple-600 to-indigo-600 border-0 w-full" />
+                          ) : (
+                            <span></span>
+                          )}
+                        </div>
+                        <p className="col-span-2 text-md mt-2">{tab.name}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className="flex flex-wrap gap-10">
+                  <button
+                    onClick={() => setShowConnectWallet(true)}
+                    className="rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700"
+                  >
+                    Get Started
+                  </button>
+                  <button
+                    onClick={() =>
+                      window.open(
+                        'https://i-am-human.gitbook.io/i-am-human-docs/',
+                        '_blank'
+                      )
+                    }
+                    className="rounded-md border border-purple-500 text-purple-500 border-1 px-4 py-2 text-base font-light text-black shadow-sm"
+                  >
+                    Learn More
+                  </button>
+                </div>
+              </div>
+              <div className="flex-1 min-w-[300px] order-first md:order-last">
+                <img src={Design} className="w-full object-fill" />
+              </div>
             </div>
+            {isSignedIn ? <Home /> : <Landing />}
           </div>
-          <div className="flex-1 min-w-[300px] order-first md:order-last">
-            <img src={Design} className="w-full object-fill" />
-          </div>
-        </div>
-        {isSignedIn ? <Home /> : <Landing />}
-      </div>
-      <PrivacyComponent />
+          <PrivacyComponent />
+        </>
+      ) : (
+        <IsSignedInLanding
+          activeTabIndex={activeTabIndex}
+          setActiveTabIndex={setActiveTabIndex}
+        />
+      )}
       <Footer />
     </div>
   );
