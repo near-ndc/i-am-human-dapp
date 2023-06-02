cda4d2bd8c82c457fb0f8feae783e4970901b759
Megha-Dev-19 <meghadev1901@gmail.com>
2023-06-02 22:44:22 +0530

    updated meta, text on start screen, removed error handling

--- src/components/common/Tabs.js
+++ src/components/common/Tabs.js
@@ -20,7 +20,7 @@ export const Tabs = ({
     claimer: wallet.accountId,
     redirect_uri: '',
   });
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const { executeRecaptcha } = useGoogleReCaptcha();
 
   async function checkIsUserVerified() {
@@ -42,158 +42,152 @@ export const Tabs = ({
     setActiveTabIndex(2);
   }
 
-  useEffect(() => {
-    const URL = window.location;
-    if (URL.href.includes('&state')) {
-      const code = new URLSearchParams(URL.search).get('code');
-      setEditableFields((d) => ({
-        ...d,
-        code,
-        redirect_uri: URL.origin,
-      }));
-    }
-  }, []);
+  // useEffect(() => {
+  //   const URL = window.location;
+  //   if (URL.href.includes('&state')) {
+  //     const code = new URLSearchParams(URL.search).get('code');
+  //     setEditableFields((d) => ({
+  //       ...d,
+  //       code,
+  //       redirect_uri: URL.origin,
+  //     }));
+  //   }
+  // }, []);
 
   useEffect(() => {
-    if (activeTabIndex === 2) {
-      checkIsUserVerified().then(() => setLoading(false));
-    } else {
-      setLoading(false);
-    }
+    // if (activeTabIndex === 2) {
+    //   checkIsUserVerified().then(() => setLoading(false));
+    // } else {
+    //   setLoading(false);
+    // }
   }, []);
 
   return (
     <>
-      {loading ? (
+      {/* {loading ? (
         <div className="h-[50vh] flex justify-center items-center gap-2">
           Loading.. <CircleSpinner size={20} color="blue" />
         </div>
       ) : (
-        <>
-          <div className="hidden md:grid grid-cols-3">
-            <div className="flex flex-col items-center py-52">
-              <div className="flex flex-col">
-                <div
-                  className={`bg-gray-100 absolute top-0 left-0 ${
-                    successSBT ? 'h-[75rem]' : 'h-full'
-                  } w-[38%] -z-50`}
-                />
-                <div className="lg:mx-auto lg:max-w-7xl w-full h-3 absolute top-0 left-1/2 transform -translate-x-1/2">
-                  <div
-                    className={`bg-purple-600 h-3 ${
-                      activeTabIndex === 0
-                        ? 'w-1/3'
-                        : tabs.length === activeTabIndex + 1
-                        ? 'w-3/3'
-                        : 'w-2/3'
-                    } rounded`}
-                  />
-                </div>
-                {tabs.map((tab, index) => (
-                  <>
-                    <div
-                      className="flex items-center cursor-default"
-                      key={index}
-                    >
-                      <div
-                        className={`rounded-full border-2 ${
-                          index <= activeTabIndex
-                            ? 'border-purple-400'
-                            : 'border-gray-300'
-                        } p-3`}
-                      >
-                        {tab.header}
-                      </div>
-                      <p
-                        className={`text-m font-medium ml-4 mr-2 ${
-                          index === activeTabIndex && 'text-purple-600'
-                        }`}
-                      >
-                        {tab.name}
-                      </p>
-                      {activeTabIndex === index && !!error ? (
-                        <Warning />
-                      ) : (
-                        activeTabIndex === index &&
-                        successSBT && <CheckCircle />
-                      )}
-                      {activeTabIndex > index && <CheckCircle />}
-                    </div>
-                    {tabs.length !== index + 1 && (
-                      <hr
-                        class={`h-8 w-0.5 ml-9 ${
-                          activeTabIndex >= index
-                            ? 'bg-gradient-to-r from-purple-600 to-indigo-600'
-                            : 'bg-gray-300'
-                        } border-0 rounded-full	my-1`}
-                      />
-                    )}
-                  </>
-                ))}
-              </div>
-            </div>
-            <div className="col-span-2 pt-32 pl-12">
-              {tabs[activeTabIndex].content}
-              <img
-                src={Logo}
-                alt="logo"
-                className="absolute bottom-6 right-0 hidden lg:block"
+        <> */}
+      <div className="hidden md:grid grid-cols-3">
+        <div className="flex flex-col items-center py-52">
+          <div className="flex flex-col">
+            <div
+              className={`bg-gray-100 absolute top-0 left-0 ${
+                successSBT ? 'h-[75rem]' : 'h-full'
+              } w-[38%] -z-50`}
+            />
+            <div className="lg:mx-auto lg:max-w-7xl w-full h-3 absolute top-0 left-1/2 transform -translate-x-1/2">
+              <div
+                className={`bg-purple-600 h-3 ${
+                  activeTabIndex === 0
+                    ? 'w-1/3'
+                    : tabs.length === activeTabIndex + 1
+                    ? 'w-3/3'
+                    : 'w-2/3'
+                } rounded`}
               />
             </div>
-          </div>
-
-          <div className="block md:hidden mt-[100px]">
-            <div
-              className="grid"
-              style={{ gridTemplateColumns: '1.2fr 1.2fr 0.75fr' }}
-            >
-              {tabs.map((tab, index) => (
-                <>
+            {tabs.map((tab, index) => (
+              <>
+                <div className="flex items-center cursor-default" key={index}>
                   <div
-                    className={
-                      'flex flex-col cursor-default ' +
-                      (index === 2 ? 'justify-self-end' : '') +
-                      (index === 1 ? 'ml-4' : '')
-                    }
-                    key={index}
+                    className={`rounded-full border-2 ${
+                      index <= activeTabIndex
+                        ? 'border-purple-400'
+                        : 'border-gray-300'
+                    } p-3`}
                   >
-                    <div className="flex gap-2">
-                      <div
-                        className={`rounded-full border-2 ${
-                          index <= activeTabIndex
-                            ? 'border-purple-400'
-                            : 'border-gray-300'
-                        } ${
-                          index <= activeTabIndex
-                            ? 'stoke-purple-400'
-                            : 'stroke-gray-300'
-                        } p-1.5`}
-                      >
-                        {tab.header}
-                      </div>
-                      {tabs.length !== index + 1 && (
-                        <div className="min-w-[50%]">
-                          <hr class="h-px my-8 bg-gradient-to-r from-purple-600 to-indigo-600 border-0 w-full" />
-                        </div>
-                      )}
-                    </div>
-                    <p
-                      className={`text-m font-medium ${
-                        index <= activeTabIndex
-                          ? 'text-purple-600'
-                          : 'text-black'
-                      }`}
-                    >
-                      {tab.name}
-                    </p>
+                    {tab.header}
                   </div>
-                </>
-              ))}
-            </div>
-            <div className="my-10 flex">{tabs[activeTabIndex].content}</div>
+                  <p
+                    className={`text-m font-medium ml-4 mr-2 ${
+                      index === activeTabIndex && 'text-purple-600'
+                    }`}
+                  >
+                    {tab.name}
+                  </p>
+                  {activeTabIndex === index && !!error ? (
+                    <Warning />
+                  ) : (
+                    activeTabIndex === index && successSBT && <CheckCircle />
+                  )}
+                  {activeTabIndex > index && <CheckCircle />}
+                </div>
+                {tabs.length !== index + 1 && (
+                  <hr
+                    class={`h-8 w-0.5 ml-9 ${
+                      activeTabIndex >= index
+                        ? 'bg-gradient-to-r from-purple-600 to-indigo-600'
+                        : 'bg-gray-300'
+                    } border-0 rounded-full	my-1`}
+                  />
+                )}
+              </>
+            ))}
           </div>
-        </>
-      )}
+        </div>
+        <div className="col-span-2 pt-32 pl-12">
+          {tabs[activeTabIndex].content}
+          <img
+            src={Logo}
+            alt="logo"
+            className="absolute bottom-6 right-0 hidden lg:block"
+          />
+        </div>
+      </div>
+
+      <div className="block md:hidden mt-[100px]">
+        <div
+          className="grid"
+          style={{ gridTemplateColumns: '1.2fr 1.2fr 0.75fr' }}
+        >
+          {tabs.map((tab, index) => (
+            <>
+              <div
+                className={
+                  'flex flex-col cursor-default ' +
+                  (index === 2 ? 'justify-self-end' : '') +
+                  (index === 1 ? 'ml-4' : '')
+                }
+                key={index}
+              >
+                <div className="flex gap-2">
+                  <div
+                    className={`rounded-full border-2 ${
+                      index <= activeTabIndex
+                        ? 'border-purple-400'
+                        : 'border-gray-300'
+                    } ${
+                      index <= activeTabIndex
+                        ? 'stoke-purple-400'
+                        : 'stroke-gray-300'
+                    } p-1.5`}
+                  >
+                    {tab.header}
+                  </div>
+                  {tabs.length !== index + 1 && (
+                    <div className="min-w-[50%]">
+                      <hr class="h-px my-8 bg-gradient-to-r from-purple-600 to-indigo-600 border-0 w-full" />
+                    </div>
+                  )}
+                </div>
+                <p
+                  className={`text-m font-medium ${
+                    index <= activeTabIndex ? 'text-purple-600' : 'text-black'
+                  }`}
+                >
+                  {tab.name}
+                </p>
+              </div>
+            </>
+          ))}
+        </div>
+        <div className="my-10 flex">{tabs[activeTabIndex].content}</div>
+      </div>
+      {/* </>
+      )} */}
     </>
   );
 };
