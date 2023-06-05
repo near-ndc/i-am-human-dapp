7c2ea58e83166010752f422541d70a24365a791c
Megha-Dev-19 <meghadev1901@gmail.com>
2023-06-05 11:55:17 +0530

    twitter text and image change

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const TWITTER_TEXT =
+  'Mint%20your%20Soul%20Bound%20Token%20verifying%20your%20proof-of-personhood%20at%20https%3A%2F%2Fi-am-human.app%2F%20and%20Make%20Your%20Voice%20heard%21%20Join%20the%20Movement%2C%20Shape%20the%20Future%2C%20and%20Build%20governance%20on%20NEAR.%C2%A0%20%23WeNeedYou%20%23WeAreCommunity%20%23NDC';
+
+export const TWITTER_URL = `https://twitter.com/intent/tweet?text=${TWITTER_TEXT}`;
