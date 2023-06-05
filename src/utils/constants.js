25b25f4e68a16b4d1f7381f79f837abe00c5d903
Megha-Dev-19 <meghadev1901@gmail.com>
2023-06-05 19:05:21 +0530

    twitter env added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,3 @@
-export const TWITTER_TEXT =
-  'Mint%20your%20Soul%20Bound%20Token%20verifying%20your%20proof-of-personhood%20at%20https%3A%2F%2Fi-am-human.app%2F%20and%20Make%20Your%20Voice%20heard%21%20Join%20the%20Movement%2C%20Shape%20the%20Future%2C%20and%20Build%20governance%20on%20NEAR.%C2%A0%20%23WeNeedYou%20%23WeAreCommunity%20%23NDC';
+export const TWITTER_TEXT = process.env.REACT_APP_TWITTER_TEXT; // using env so that marketing team is able to change it frequently
 
 export const TWITTER_URL = `https://twitter.com/intent/tweet?text=${TWITTER_TEXT}`;
