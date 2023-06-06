ee28f151bd12ed98012458cc5427528df171bef4
Andrew <saychuk.andriy@gmail.com>
2023-06-06 23:40:01 +0300

    GA React.js tracking issue (#109)
    Signed-off-by: Andrew <saychuk.andriy@gmail.com>
    Co-authored-by: Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'regenerator-runtime/runtime';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import {
   BrowserRouter as Router,
@@ -7,30 +7,18 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom';
-import ReactGA from 'react-ga';
-
 import { IndexPage } from './pages/index';
 import { CommunityApplicationPage } from './pages/communtiyapplication';
 import { ScoreboardPage } from './pages/scoreboard';
-import RouteChangeTracker from './utils/routeChangeTracker';
 
 const RedirectComponent = () => {
   return <Redirect to="/" />;
 };
 
 export function App({ isSignedIn }) {
-  useEffect(() => {
-    if (
-      process.env.REACT_APP_ENV === 'prod' ||
-      process.env.REACT_APP_ENV === 'dev'
-    )
-      ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
-  }, []);
-
   return (
     <>
       <Router>
-        <RouteChangeTracker />
         <Switch>
           <Route exact path="/community-application">
             <CommunityApplicationPage isSignedIn={isSignedIn} />
