f6163dd825f9f56c5b5950d5ac0e3582d9f3f42c
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2023-06-07 14:42:31 +0530

    Bug Fix: Success SBT Page Redirect (#111)

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,7 @@
 export const TWITTER_TEXT = process.env.REACT_APP_TWITTER_TEXT; // using env so that marketing team is able to change it frequently
 
 export const TWITTER_URL = `https://twitter.com/intent/tweet?text=${TWITTER_TEXT}`;
+
+export const LSKeys = {
+  SHOW_SBT_PAGE: 'showSuccessSBTPage',
+};
