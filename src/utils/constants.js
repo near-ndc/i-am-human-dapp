52e6799818676ec5fd159473fc7fd361c023d530
Megha-Dev-19 <meghadev1901@gmail.com>
2023-06-08 21:38:24 +0530

    added code to add existing users

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,15 @@
+import moment from 'moment';
+
 export const TWITTER_TEXT = process.env.REACT_APP_TWITTER_TEXT; // using env so that marketing team is able to change it frequently
 
 export const TWITTER_URL = `https://twitter.com/intent/tweet?text=${TWITTER_TEXT}`;
 
 export const LSKeys = {
   SHOW_SBT_PAGE: 'showSuccessSBTPage',
+};
+
+// used in supabase column type for date
+export const convertToTimestamptz = (timestamp) => {
+  console.log(timestamp);
+  return moment.utc(timestamp).format('YYYY-MM-DDTHH:mm:ssZ');
 };
