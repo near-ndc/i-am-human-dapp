7c250d38fdc91248d39347eb01a3c8e2afe4ea99
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2023-06-09 23:53:23 +0530

    Ft: Store existing users data (#113)
    Signed-off-by: Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,16 @@
+import moment from 'moment';
+
 export const TWITTER_TEXT = process.env.REACT_APP_TWITTER_TEXT; // using env so that marketing team is able to change it frequently
 
 export const TWITTER_URL = `https://twitter.com/intent/tweet?text=${TWITTER_TEXT}`;
 
 export const LSKeys = {
   SHOW_SBT_PAGE: 'showSuccessSBTPage',
+};
+
+// used in supabase column type for date
+export const convertToTimestamptz = (timestamp) => {
+  return moment.utc(timestamp).format('YYYY-MM-DDTHH:mm:ssZ');
 };
 
 export function hasTwoDots(str) {
