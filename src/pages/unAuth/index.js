be111f84b5dc4ddfb5a50cd7304ee8ad8d7b7163
Megha-Dev-19 <meghadev1901@gmail.com>
2023-06-16 19:48:06 +0530

    og sbt added

--- src/pages/unAuth/index.js
+++ src/pages/unAuth/index.js
@@ -37,62 +37,6 @@ export const Landing = () => {
       </div>
       <div className="flex flex-wrap gap-10">
         <div className="flex-1 min-w-[300px] md:mt-10">
-          <h2 className="font-bold text-4xl">Get Your KYC Soul Bound Token</h2>
-          <p className="my-5">
-            Have you already KYC with Fractal? You are in luck. If you have
-            received a bounty payout for your contribution from the NEAR
-            Foundation, you will not be required to re-verify your identity when
-            minting your SBTs. Mint your SBTs now.
-          </p>
-          <button
-            onClick={() =>
-              window.open(
-                'https://i-am-human.gitbook.io/i-am-human-docs/kyc-sbt',
-                '_blank'
-              )
-            }
-            className="inline-flex rounded-md border border-purple-500 text-purple-500 border-1 px-5 py-2 md:mt-5 text-base font-light text-black shadow-sm"
-          >
-            Learn More
-          </button>
-        </div>
-        <div className="md:min-w-[400px] order-first w-full md:w-1/3 flex justify-center md:justify-start">
-          <img src={KYCSBTImage} className="object-fill rounded-lg md:w-2/3" />
-        </div>
-      </div>
-      <div className="flex flex-wrap gap-10">
-        <div className="flex-1 min-w-[300px] md:mt-10">
-          <h2 className="font-bold text-4xl">
-            Get Your Community Soul Bound Token
-          </h2>
-          <p className="my-5">
-            Are you part of the Community? If you are active in the NEAR
-            Community, we will be introducing Community SBT soon. Mint your
-            Community SBT and join us to build web3 governance.
-          </p>
-
-          <button
-            onClick={() =>
-              window.open(
-                'https://i-am-human.gitbook.io/i-am-human-docs/community-sbt',
-                '_blank'
-              )
-            }
-            className="cursor-auto inline-flex rounded-md border border-gray-500 border-1 px-5 py-2 md:mt-5 text-base font-light text-black shadow-sm"
-          >
-            Coming Soon
-          </button>
-        </div>
-        <div className="md:min-w-[400px] order-first md:order-last w-full md:w-1/3 flex justify-center md:justify-end">
-          <img
-            src={CommunitySBTImage}
-            className="object-fill rounded-lg md:w-2/3"
-          />
-        </div>
-      </div>
-
-      <div className="flex flex-wrap gap-10">
-        <div className="flex-1 min-w-[300px] md:mt-10">
           <h2 className="font-bold text-4xl">Get Your OG Soul Bound Token</h2>
           <p className="my-5">
             Are you someone who stands out in the NEAR ecosystem? Get the OG
@@ -103,6 +47,61 @@ export const Landing = () => {
             onClick={() =>
               window.open(
                 'https://i-am-human.gitbook.io/i-am-human-docs/og-sbt',
+
+                '_blank'
+              )
+            }
+            className="inline-flex rounded-md border border-purple-500 text-purple-500 border-1 px-5 py-2 md:mt-5 text-base font-light text-black shadow-sm"
+          >
+            Learn More
+          </button>
+        </div>
+        <div className="md:min-w-[400px] order-first w-full md:w-1/3 flex justify-center md:justify-start">
+          <img src={OGSBT} className="object-fill rounded-lg md:w-2/3" />
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-10">
+        <div className="flex-1 min-w-[300px] md:mt-10">
+          <h2 className="font-bold text-4xl">Get Your KYC Soul Bound Token</h2>
+          <p className="my-5">
+            Have you already KYC with Fractal? You are in luck. If you have
+            received a bounty payout for your contribution from the NEAR
+            Foundation, you will not be required to re-verify your identity when
+            minting your SBTs. Mint your SBTs now.
+          </p>
+
+          <button
+            onClick={() =>
+              window.open(
+                'https://i-am-human.gitbook.io/i-am-human-docs/kyc-sbt',
+
+                '_blank'
+              )
+            }
+            className="inline-flex rounded-md border border-purple-500 text-purple-500 border-1 px-5 py-2 md:mt-5 text-base font-light text-black shadow-sm"
+          >
+            Learn More
+          </button>
+        </div>
+        <div className="md:min-w-[400px] order-first md:order-last w-full md:w-1/3 flex justify-center md:justify-end">
+          <img src={KYCSBTImage} className="object-fill rounded-lg md:w-2/3" />
+        </div>
+      </div>
+
+      <div className="flex flex-wrap gap-10">
+        <div className="flex-1 min-w-[300px] md:mt-10">
+          <h2 className="font-bold text-4xl">
+            Get Your Community Soul Bound Token
+          </h2>
+          <p className="my-5">
+            Are you part of the Community? If you are active in the NEAR
+            Community, we will be introducing Community SBT soon. Mint your
+            Community SBT and join us to build web3 governance.
+          </p>
+          <button
+            onClick={() =>
+              window.open(
+                'https://i-am-human.gitbook.io/i-am-human-docs/community-sbt',
                 '_blank'
               )
             }
@@ -112,7 +111,10 @@ export const Landing = () => {
           </button>
         </div>
         <div className="md:min-w-[400px] order-first w-full md:w-1/3 flex justify-center md:justify-start">
-          <img src={OGSBT} className="object-fill rounded-lg md:w-2/3" />
+          <img
+            src={CommunitySBTImage}
+            className="object-fill rounded-lg md:w-2/3"
+          />
         </div>
       </div>
     </div>
