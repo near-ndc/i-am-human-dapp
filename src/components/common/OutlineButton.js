abf923eaa3e751515baf741e46d99b310ef68499
Megha-Dev-19 <meghadev1901@gmail.com>
2023-07-18 23:16:28 +0530

    fixed burn

--- src/components/common/OutlineButton.js
+++ src/components/common/OutlineButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const OutlineButton = ({ onClick, children, classes }) => {
+export const OutlineButton = ({ onClick, children, classes, disabled }) => {
   return (
     <button
       type="button"
@@ -9,6 +9,7 @@ export const OutlineButton = ({ onClick, children, classes }) => {
         `rounded-md border border-gray-500 border-1 px-5 text-center py-2 font-light text-black shadow-sm ` +
         classes
       }
+      disabled={disabled}
     >
       {children}
     </button>
