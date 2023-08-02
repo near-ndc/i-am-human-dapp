fe05c1a48f4081b4a65b0e5b5f602d28275e19b7
Megha-Dev-19 <meghadev1901@gmail.com>
2023-08-02 20:31:35 +0530

    debugging changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -318,7 +318,7 @@ const IndexPage = () => {
                             onClick={() => dispatch(setActivePageIndex(1))}
                             className="rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-3 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700"
                           >
-                            Get Your FV Token
+                            Get Your FV SBT
                           </button>
                         )}
                         <button
